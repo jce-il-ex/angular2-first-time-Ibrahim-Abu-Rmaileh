41fd98a4dbe3b72bccbdc2a90058baecbdfe19ba
Ibrahim-Abu-Rmaileh <ibrahim.abu.rmaileh@gmail.com>
2017-03-21 09:26:46 +0000

    ddd

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppListComponent } from './app-list/app-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppListComponent
   ],
   imports: [
     BrowserModule,
